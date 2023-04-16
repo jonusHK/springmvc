bad70087a0e0f724aeed23c87e186c95dd152600
jonusHK <positipman@gmail.com>
2023-04-17 00:11:51 +0900

    Add: 로깅

--- src/main/kotlin/com/dhkpo/springmvc/basic/LogTestController.kt
+++ src/main/kotlin/com/dhkpo/springmvc/basic/LogTestController.kt
@@ -1,4 +1,25 @@
 package com.dhkpo.springmvc.basic
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class LogTestController {
+
+    private val log: Logger = LoggerFactory.getLogger(this.javaClass)
+
+    @RequestMapping("/log-test")
+    fun logTest(): String {
+        val name = "Spring"
+
+        log.trace("trace log = {}", name)
+        log.debug("debug log = {}", name)
+        log.info("info log = {}", name)
+        log.warn("warn log = {}", name)
+        log.error("error log = {}", name)
+
+        return "ok"
+    }
 }
